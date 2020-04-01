13c557e8617672c7be66140dd2c1d712716605f3
kyoncy <kyota.n.2014@gmail.com>
2020-04-01 22:12:02 +0900

    :sparkles: able to upload cropped file

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,22 +1,26 @@
 import React, { useState, useRef } from "react";
 import Cropper from "react-cropper";
-// import { storage, db } from "../src/firebase";
+import { storage, db } from "../src/firebase";
 
-// const removeExtension = (str: string) => {
-//   let base = new String(str).substring(str.lastIndexOf("/") + 1);
-//   if (base.lastIndexOf(".") != -1) {
-//     base = base.substring(0, base.lastIndexOf("."));
-//   }
-//   return base;
-// };
+const removeExtension = (str: string) => {
+  let base = new String(str).substring(str.lastIndexOf("/") + 1);
+  if (base.lastIndexOf(".") != -1) {
+    base = base.substring(0, base.lastIndexOf("."));
+  }
+  return base;
+};
 
 const FileUpload: React.FC = () => {
   const [src, setSrc] = useState("");
+  const [name, setName] = useState("");
+  const [croppedBlob, setCroppedBlob] = useState<any>(null);
   const cropper: any = useRef(null);
 
   const _crop = () => {
     if (cropper) {
-      console.log(cropper.current.getCroppedCanvas().toDataURL());
+      cropper.current.getCroppedCanvas().toBlob((blob: Blob) => {
+        setCroppedBlob(blob);
+      });
     }
   };
 
@@ -24,24 +28,40 @@ const FileUpload: React.FC = () => {
     const { files } = event.target;
     if (files.length === 1) {
       setSrc(window.URL.createObjectURL(files[0]));
-      // const file = files[0];
-      // const storageRef = storage.ref();
-      // const ref = storageRef.child(file.name);
-      // ref.put(file).then(snapshot => {
-      //   console.log("Uploaded a blob or file!");
-      //   db.collection("emojis")
-      //     .doc()
-      //     .set({
-      //       image: `gs://${snapshot.ref.bucket}/${snapshot.ref.name}`,
-      //       name: removeExtension(file.name)
-      //     });
-      // });
+      setName(files[0].name);
+    }
+  };
+
+  const blobToFile = (): File => {
+    croppedBlob.lastModifiedDate = new Date();
+    croppedBlob.name = name;
+    return croppedBlob as File;
+  };
+
+  const uploadFile = () => {
+    if (croppedBlob) {
+      const file: File = blobToFile();
+      const storageRef = storage.ref();
+      const ref = storageRef.child(file.name);
+      ref.put(file).then(snapshot => {
+        console.log("Uploaded a blob or file!");
+        db.collection("emojis")
+          .doc()
+          .set({
+            image: `gs://${snapshot.ref.bucket}/${snapshot.ref.name}`,
+            name: removeExtension(file.name)
+          })
+          .then(() => {
+            location.reload();
+          });
+      });
     }
   };
 
   return (
     <div style={{ marginBottom: "10px" }}>
       <input type="file" accept="image/jpeg,image/png" onChange={selectFile} />
+      {src ? <button onClick={uploadFile}>アップロード</button> : null}
       <Cropper
         ref={cropper}
         src={src}
